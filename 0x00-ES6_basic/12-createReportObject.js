344c2a8e675976e5450fa3173a82349456d71842
idrissjuanito <idrissjuanito@gmail.com>
2024-03-04 20:32:56 +0100

    js nested objects

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,6 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(list) { return Object.keys(list).length; },
+  };
+}
