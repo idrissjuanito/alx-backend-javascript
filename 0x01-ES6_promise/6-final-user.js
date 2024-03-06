edb839d573ae69b7b5dd7a33cc32c0869cd95254
idrissjuanito <idrissjuanito@gmail.com>
2024-03-06 15:43:10 +0100

    ES6 Promises and async with js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,8 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+const signUpUser = require('./4-user-promise');
+const uploadPhoto = require('./5-photo-reject');
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
-  const filePromise = uploadPhoto(filename);
-  const allPromises = Promise.allSettled([userPromise, filePromise]);
-  return allPromises;
+  const filePromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, filePromise]);
 }
