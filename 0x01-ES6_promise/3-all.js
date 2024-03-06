d8a8672819ac3dc1ca04fe2d666a50fcce644289
idrissjuanito <idrissjuanito@gmail.com>
2024-03-06 17:22:11 +0100

    ES6 Promises and async with js 5

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -8,5 +8,3 @@ export default function handleProfileSignup() {
     console.log(results[0].body, results[1].firstName, results[1].lastName);
   });
 }
-
-handleProfileSignup();
--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,5 +1,3 @@
-import guardrail from './9-try';
-import divideFunction from './8-try';
+import handleProfileSignup from "./3-all";
 
-console.log(guardrail(() => { return divideFunction(10, 2); }));
-console.log(guardrail(() => { return divideFunction(10, 0); }));
+handleProfileSignup();
