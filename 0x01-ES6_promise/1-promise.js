1a8121cff48efbb5263f9d6a11214a0f204cb137
idrissjuanito <idrissjuanito@gmail.com>
2024-03-06 15:54:56 +0100

    ES6 Promises and async with js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) return resolve({ status: 200, body: 'success' });
+    if (success) {
+      return resolve({ status: 200, body: 'success' });
+    }
     return reject(new Error('The fake API is not working currently'));
   });
 }
