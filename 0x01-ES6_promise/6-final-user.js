706aa195472fce94f23aa77c3039e2a54129df4b
idrissjuanito <idrissjuanito@gmail.com>
2024-03-06 15:08:44 +0100

    ES^ Promises task 1 to 9

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const usePromise = signUpUser(firstName, lastName);
+  const userPromise = signUpUser(firstName, lastName);
   const filePromise = uploadPhoto(filename);
-  const allPromises = Promise.allSettled([usePromise, filePromise]);
+  const allPromises = Promise.allSettled([userPromise, filePromise]);
   return allPromises;
 }
