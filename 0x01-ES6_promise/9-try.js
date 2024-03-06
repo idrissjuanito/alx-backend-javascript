b63e7e6c271867c8855a8c4176081fac72b67f37
idrissjuanito <idrissjuanito@gmail.com>
2024-03-06 17:16:50 +0100

    ES6 Promises and async with js 4

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const res = mathFunction();
     queue.push(res);
   } catch (err) {
-    queue.push(err.message);
+    queue.push(`Error: ${err.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
