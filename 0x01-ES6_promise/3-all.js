b1bf19c1cfe8afa5617c98ef02ddbedf054f5ee1
idrissjuanito <idrissjuanito@gmail.com>
2024-03-06 16:23:03 +0100

    ES6 Promises and async with js 2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 const { uploadPhoto, createUser } = require('./utils');
 
-export default function handleProfileSignup() {
+exports.default = function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
   const allPromises = Promise.all([photo, user]);
   allPromises.then((results) => {
     console.log(results[0].body, results[1].firstName, results[1].lastName);
   });
-}
+};
