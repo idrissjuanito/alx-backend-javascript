9ce250fdd71f110bece13b4e23f0702515064963
idrissjuanito <idrissjuanito@gmail.com>
2024-03-07 21:50:46 +0100

    ES6 inheritance

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,18 +1,13 @@
-import HolbertonCourse from './2-hbtn_course';
+import Building from './5-building';
 
-const c1 = new HolbertonCourse('ES6', 1, ['Bob', 'Jane']);
-console.log(c1.name);
-c1.name = 'Python 101';
-console.log(c1);
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
 
 try {
-  c1.name = 12;
-} catch (err) {
-  console.log(err);
+    new TestBuilding(200)
 }
-
-try {
-  const c2 = new HolbertonCourse('ES6', '1', ['Bob', 'Jane']);
-} catch (err) {
-  console.log(err);
+catch(err) {
+    console.log(err);
 }
