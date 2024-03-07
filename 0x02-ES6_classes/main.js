7279ba99b9baae5cb82171906b9831645d3d5bd0
idrissjuanito <idrissjuanito@gmail.com>
2024-03-08 00:09:39 +0100

    ES6 classes, inheritance and abstract

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,17 +1,5 @@
-import Building from './5-building';
+import Airport from "./7-airport.js";
 
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {
-  evacuationWarningMessage() {
-    console.log('Here you')
-  }
-}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
