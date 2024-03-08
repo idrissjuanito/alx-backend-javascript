730afdf85cbb0a43eca6805c090c2466740a92c2
idrissjuanito <idrissjuanito@gmail.com>
2024-03-08 02:23:04 +0100

    ES6 classes prototype chains

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -29,7 +29,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.local}`;
+    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.local}`;
   }
 }
 
