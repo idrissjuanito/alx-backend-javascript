22d79c4325f1db274a3a232b889c43112a653b7d
idrissjuanito <idrissjuanito@gmail.com>
2024-03-12 00:41:27 +0100

    js array buffers

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw new Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
   const typedArr = new Int8Array(buffer);
