163a43df8ed3ad8e3ce6b19ed639ec02f9d721a1
idrissjuanito <idrissjuanito@gmail.com>
2024-03-12 00:09:01 +0100

    js maps setting valuees

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,15 @@ export default function updateUniqueItems(map) {
   const mp = map;
   if (!(map instanceof Map)) throw new Error('Cannot process');
   map.forEach((val, key) => {
-    if (val === 1) mp[key] = 100;
+    if (val === 1) mp.set(key, 100);
   });
   return mp;
 }
+
+import groceriesList from "./9-groceries_list.js";
+
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map)
+console.log(map);
