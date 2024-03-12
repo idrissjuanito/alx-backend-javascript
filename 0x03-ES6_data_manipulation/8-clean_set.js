bf67f41954a9ba4eeaa74919682651d73c8d4e46
idrissjuanito <idrissjuanito@gmail.com>
2024-03-12 21:59:49 +0100

    task 8 correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,12 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0) return '';
   const str = [];
   set.forEach((elm) => {
-    if (elm.startsWith(startString)) {
+    if (elm.startsWith(startString) && elm.length > startString.length) {
       str.push(elm.slice(startString.length));
     }
   });
   return str.join('-');
 }
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana', 'bon']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
