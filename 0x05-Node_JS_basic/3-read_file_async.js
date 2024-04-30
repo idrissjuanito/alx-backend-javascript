e0fe4df6488a9e6dd850ec47fdb63d31633d68ca
idrissjuanito <idrissjuanito@gmail.com>
2024-04-30 19:50:01 +0100

    reading files in node

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -32,7 +32,7 @@ async function countStudents(path) {
       console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
     }
   } catch (e) {
-    throw new Error('Cannot load the database');
+    throw new Error('Cnot load the database');
   }
 }
 
