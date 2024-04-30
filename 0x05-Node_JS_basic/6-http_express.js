8acba2b4dbfacfb92227efcf2f86a98acf5a4da8
idrissjuanito <idrissjuanito@gmail.com>
2024-04-30 19:45:10 +0100

    nodejs express app 1

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -7,18 +7,5 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('*', (req, res) => {
-  res.send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Error</title>
-</head>
-<body>
-<pre>Cannot GET /lskdlskd</pre>
-</body>
-</html> `);
-});
-
 app.listen(1245);
 module.exports = app;
