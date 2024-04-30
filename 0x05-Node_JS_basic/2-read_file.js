15b996093e4106e8713e46071bda175101401e81
idrissjuanito <idrissjuanito@gmail.com>
2024-04-30 20:17:06 +0100

    reading files in node 4

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,6 +2,9 @@
 const fs = require('fs');
 
 function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
   const content = fs.readFileSync(path).toString();
   let studentCount = 0;
   let line = '';
