ddf1a7b571f1ff6264c94473659e81639d4338b5
idrissjuanito <idrissjuanito@gmail.com>
2024-04-30 08:50:30 +0100

    node file system async reading

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,18 +9,20 @@ function countStudents(path) {
   const studentsByField = {};
   for (let i = 0; i < content.length; i += 1) {
     if (content[i] === '\n') {
-      studentCount += 1;
-      if (studentCount > 1) {
-        const studentData = line.split(',');
-        const field = studentData[studentData.length - 1];
-        const fields = Object.keys(studentsByField);
-        if (fields.includes(field)) {
-          studentsByField[field].push(studentData[0]);
-        } else {
-          studentsByField[field] = [studentData[0]];
+      if (line.length > 1) {
+        studentCount += 1;
+        if (studentCount > 1) {
+          const studentData = line.split(',');
+          const field = studentData[studentData.length - 1];
+          const fields = Object.keys(studentsByField);
+          if (fields.includes(field)) {
+            studentsByField[field].push(studentData[0]);
+          } else {
+            studentsByField[field] = [studentData[0]];
+          }
         }
+        line = '';
       }
-      line = '';
     } else {
       line += content[i];
     }
