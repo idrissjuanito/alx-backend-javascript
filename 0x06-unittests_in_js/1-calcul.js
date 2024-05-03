c0cc2d3baafde23711b4c71144885159ece8542e
idrissjuanito <idrissjuanito@gmail.com>
2024-05-03 12:38:56 +0100

    Nodejs tests subtests with describe 3

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ function calculateNumber(type, a, b) {
   const x = Math.round(a);
   const y = Math.round(b);
   if (type === 'SUM') return x + y;
-  if (type === 'SUBSTRACT') return x - y;
+  if (type === 'SUBTRACT') return x - y;
   if (type === 'DIVIDE') {
     if (y === 0) return 'Error';
     return x / y;
